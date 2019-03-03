11f67b259a1be2f480b737c7adfde951e7038a56
ranbo1205 <yangsheng122000@qq.com>
2019-03-03 14:09:23 +0800

    uplaod local project to git

---
+++ src/main/java/com/jk/heng/HengApplication.java
@@ -0,0 +1,13 @@
+package com.jk.heng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HengApplication.class, args);
+    }
+
+}
