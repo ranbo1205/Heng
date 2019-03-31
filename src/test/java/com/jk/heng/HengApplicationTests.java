ad85a4095ac39b4d56ffa1e9971457204d32ad26
ranbo1205 <yangsheng122000@qq.com>
2019-03-31 17:18:01 +0800

    add example for java 异步调用及exception handle
    add example for java 异步调用及exception handle

--- src/test/java/com/jk/heng/HengApplicationTests.java
+++ src/test/java/com/jk/heng/HengApplicationTests.java
@@ -1,16 +1,28 @@
 package com.jk.heng;
 
+import com.jk.heng.route.exceptions.AsyncExceptionDemo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HengApplicationTests {
 
+    @Autowired
+    private AsyncExceptionDemo asyncDemo;
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws ExecutionException, InterruptedException {
+        asyncDemo.asyncInvokeSimplest();
+        asyncDemo.asyncInvokeWithException("test Exception thrown from void ");
+        Future<String> future = asyncDemo.asyncInvokeWithFuture(100);
+        System.out.println(future.get());
     }
 
 }
